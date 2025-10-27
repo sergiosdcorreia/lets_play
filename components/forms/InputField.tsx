e0c9266605a2e9fb5c0ae83d886e8991efdd966d
Sergio Correia <sergiosdcorreia@gmail.com>
2025-10-27 16:22:41 +0000

    Installing ShadCN

--- components/forms/InputField.tsx
+++ components/forms/InputField.tsx
@@ -1,4 +1,14 @@
-const InputField = () => {
+const InputField = ({
+  name,
+  label,
+  placeholder,
+  type = "text",
+  register,
+  error,
+  validation,
+  disabled,
+  value,
+}: FormInputProps) => {
   return (
     <div className="space-y-2">
       <label>label</label>
--- types/global.d.ts
+++ types/global.d.ts
@@ -9,6 +9,18 @@ declare global {
     email: string;
     password: string;
   };
+
+  type FormInputProps = {
+    name: string;
+    label: string;
+    placeholder: string;
+    type?: string;
+    register: UseFormRegister;
+    error?: FieldError;
+    validation?: RegisterOptions;
+    disabled?: boolean;
+    value?: string;
+  };
 }
 
 export {};
